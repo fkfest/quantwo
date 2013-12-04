14f3403de05613ca23b9ebee411fdb01811ffd8b
Daniel Kats <kats@theochem.uni-stuttgart.de>
2013-12-04 13:43:49 +0100

    arrays files

---
+++ src/arrays.cpp
@@ -0,0 +1,70 @@
+#include "arrays.h"
+template <class T>
+inline
+void Array<T>::identity(unsigned long int n)
+{
+  this->reserve(n);
+  for ( uint i = 0; i < n; ++i ) this->push_back(i);
+}
+template <class T>
+inline
+bool Array<T>::is_identity() const
+{
+  for ( uint i = 0; i < this->size(); ++i ) 
+    if ( (*this)[i] != i ) return false;
+  return true;
+}
+template <class T>
+inline
+Array<T> Array<T>::subarray(unsigned long int beg, unsigned long int end) const
+{
+  unsigned long int end1=(end < this->size() ? end + 1 : this->size());
+  return Array<T>(this->begin()+beg,this->begin()+end1);
+}
+template <class T>
+inline
+int Array<T>::find(const T& t, uint ipos) const
+{
+  long unsigned int i = ipos;
+  for ( typename Array<T>::const_iterator it = this->begin()+ipos; it != this->end(); ++it, ++i )
+    if ( t== *it ) return i;
+  return -1;
+}
+template <class T>
+inline
+void Array<T>::resort()
+{
+  std::sort(this->begin(), this->end());
+}
+template <class T>
+inline
+Array<T> Array<T>::refarr( const Array<uint>& ref ) const
+{
+  Array<T> res;
+  Array<uint>::const_iterator ir;
+  _foreach(ir,ref){
+    assert( *ir < this->size() );
+    res.push_back((*this)[*ir]);
+  }
+  return res;
+}
+template <class T>
+inline
+void Array<T>::canonicalize()
+{
+  typename Array<T>::iterator it;
+  _foreach(it,*this){
+    it->canonicalize();
+  }
+  std::sort(this->begin(), this->end());
+}
+
+template <class T>
+inline
+std::ostream & operator << (std::ostream & o, Array<T> const & p)
+{
+  for ( typename Array<T>::const_iterator i=p.begin(); i!=p.end(); ++i )
+    o << *i;
+  return o;
+}
+
---
+++ src/arrays.h
@@ -0,0 +1,40 @@
+#ifndef Array_H
+#define Array_H
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <assert.h>
+#include "utilities.h"
+
+
+/*
+some useful functions for vector arrays
+*/
+template <class T>
+class Array : public std::vector<T> {
+  public:
+    Array<T> () : std::vector<T>(){};
+    Array<T> ( typename Array<T>::const_iterator beg, typename Array<T>::const_iterator end)
+     : std::vector<T>(beg,end){};
+    Array<T> ( unsigned long int size ) : std::vector<T>(size){};
+    // set from 0 to n
+    void identity( unsigned long int n );
+    bool is_identity() const;
+    // get sub array 
+    Array<T> subarray(unsigned long int beg, unsigned long int end) const;
+    // search (starting from position ipos), if not found -> -1
+    int find(T const & t, uint ipos = 0) const;
+    // quick sort in increasing order
+    void resort();
+    // referenced array ((*this)[ref[i]])
+    Array<T> refarr( const Array<uint>& ref ) const;
+    // canonicalize each element and sort in increasing order
+    void canonicalize();
+};
+template <class T>
+std::ostream & operator << (std::ostream & o, Array<T> const & p);
+
+#include "arrays.cpp"
+
+#endif
+
